e86c4dadffcd5e0d504c22f3615199be53b862e8
Maksym Dmytryshyn <dmytryshyn_max@ukr.net>
2021-04-24 18:45:21 +0300

    Add page

--- src/App.js
+++ src/App.js
@@ -13,22 +13,20 @@ import GalleryPage from "./pages/GalleryPage.js";
 import ContactsPage from "./pages/ContactsPage.js";
 
 function App() {
-  let { path } = useRouteMatch();
-
   return (
     <div className="App">
       <Router>
         <header className="nav-header">
-            <NavLink className="nav-link" activeClassName="active-nav" to={path}>Home</NavLink>
-            <NavLink className="nav-link" activeClassName="active-nav" to={`${path}/posts`}>Posts</NavLink>
+            <NavLink className="nav-link" activeClassName="active-nav" to="/21-router">Home</NavLink>
+            <NavLink className="nav-link" activeClassName="active-nav" to="/posts">Posts</NavLink>
             <NavLink className="nav-link" activeClassName="active-nav" to="/gallery">Gallery</NavLink>
             <NavLink className="nav-link" activeClassName="active-nav" to="/contacts">Contacts</NavLink>
         </header>
           <Switch>
-            <Route exact path={path}>
+            <Route exact path="/21-router">
               <HomePage/>
             </Route>
-            <Route exact path={`${path}/posts`}>
+            <Route exact path="/posts">
               <PostPage/>
             </Route>
             <Route exact path="/gallery">
